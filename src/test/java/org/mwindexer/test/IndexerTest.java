ba015fded5864a01464ff9db19c795fd1524f39a
Tim Stoner <timothy.m.stoner@gmail.com>
2014-04-24 14:24:08 -0400

    added some testing features

--- src/test/java/org/mwindexer/test/IndexerTest.java
+++ src/test/java/org/mwindexer/test/IndexerTest.java
@@ -2,40 +2,40 @@ package org.mwindexer.test;
 
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
+import org.apache.solr.core.CoreContainer;
 import org.apache.solr.util.AbstractSolrTestCase;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mwindexer.Indexer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class IndexerTest {
 
+	private static Logger LOG = LoggerFactory.getLogger(IndexerTest.class);
+
 	private SolrServer server;
-
-	public static String getSchemaFile() {
-		return "conf/schema.xml";
-	}
-
-	public static String getSolrConfigFile() {
-		return "conf/solrconfig.xml";
-	}
 
 	@Before
 	public void setUp() throws Exception {
-//		server = new EmbeddedSolrServer(h.getCoreContainer(), h.getCore()
-//				.getName());
+		LOG.info("setting up");
+		CoreContainer container = new CoreContainer("src/test/resources/solr");
+		server = new EmbeddedSolrServer(container, "core1");
 	}
 
 	@After
 	public void shutDown() {
-//		server.shutdown();
+		LOG.info("shutting down");
+		server.shutdown();
 	}
 
 	@Test
 	public void testIndexer() {
-		String args[] = { "-i", "sample.xml" };
+		LOG.info("testing indexer");
+		String args[] = { "-i", "src/test/resources/sample.xml" };
 
-//		Indexer indexer = new Indexer(args);
+		 Indexer indexer = new Indexer(args);
 
 	}
 
