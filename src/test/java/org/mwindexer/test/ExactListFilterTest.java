0a01bf2af6a454d9febbbcde645f921bf5db4e91
test 0101 <test72157@gmail.com>
2014-04-25 12:35:31 -0400

    some more filter tests

--- src/test/java/org/mwindexer/test/ExactListFilterTest.java
+++ src/test/java/org/mwindexer/test/ExactListFilterTest.java
@@ -3,34 +3,39 @@ package org.mwindexer.test;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
-import java.text.ParseException;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mwindexer.filters.NotalkFilter;
+import org.mwindexer.filters.ExactListFilter;
 import org.mwindexer.indexer.NamespaceSet;
 import org.mwindexer.model.Page;
 import org.mwindexer.model.Title;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class NotalkFilterTest {
-	private static Logger LOG = LoggerFactory.getLogger(NotalkFilterTest.class);
+public class ExactListFilterTest {
+	private static Logger LOG = LoggerFactory
+			.getLogger(ExactListFilterTest.class);
 
-	private NotalkFilter filter;
+	private ExactListFilter filter;
 
 	private MockDumpWriter writer;
 
 	private NamespaceSet namespaceSet;
 
 	@Before
-	public void setUp() throws ParseException {
+	public void setup() {
 		namespaceSet = new NamespaceSet();
 		namespaceSet.add(0, "");
 		namespaceSet.add(1, "Talk");
 
 		writer = new MockDumpWriter();
-		filter = new NotalkFilter(writer);
+		try {
+			filter = new ExactListFilter(writer,
+					"src/test/resources/filters/exactlistfilter.txt");
+		} catch (IOException e) {
+			LOG.error("Problem reading exact list filter data", e);
+		}
 	}
 
 	@Test
@@ -47,9 +52,39 @@ public class NotalkFilterTest {
 
 		assertEquals(true, writer.isPagePassed());
 	}
+	
+	@Test
+	public void testTalkPagePermitted() {
+		Page page = new Page();
+		page.Title = new Title(1, "permitted", namespaceSet);
+
+		try {
+			filter.writeStartPage(page);
+			filter.writeEndPage();
+		} catch (IOException e) {
+			LOG.error("Problem writing revision", e);
+		}
+
+		assertEquals(false, writer.isPagePassed());
+	}
 
 	@Test
 	public void testPageFiltered() {
+		Page page = new Page();
+		page.Title = new Title(0, "filtered", namespaceSet);
+
+		try {
+			filter.writeStartPage(page);
+			filter.writeEndPage();
+		} catch (IOException e) {
+			LOG.error("Problem writing revision", e);
+		}
+
+		assertEquals(false, writer.isPagePassed());
+	}
+
+	@Test
+	public void testTalkPageFiltered() {
 		Page page = new Page();
 		page.Title = new Title(1, "filtered", namespaceSet);
 
@@ -62,4 +97,5 @@ public class NotalkFilterTest {
 
 		assertEquals(false, writer.isPagePassed());
 	}
+
 }
--- src/test/java/org/mwindexer/test/ListFilterTest.java
+++ src/test/java/org/mwindexer/test/ListFilterTest.java
@@ -3,34 +3,38 @@ package org.mwindexer.test;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
-import java.text.ParseException;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mwindexer.filters.NotalkFilter;
+import org.mwindexer.filters.ListFilter;
 import org.mwindexer.indexer.NamespaceSet;
 import org.mwindexer.model.Page;
 import org.mwindexer.model.Title;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class NotalkFilterTest {
-	private static Logger LOG = LoggerFactory.getLogger(NotalkFilterTest.class);
+public class ListFilterTest {
+	private static Logger LOG = LoggerFactory.getLogger(ListFilterTest.class);
 
-	private NotalkFilter filter;
+	private ListFilter filter;
 
 	private MockDumpWriter writer;
 
 	private NamespaceSet namespaceSet;
 
 	@Before
-	public void setUp() throws ParseException {
+	public void setup() {
 		namespaceSet = new NamespaceSet();
 		namespaceSet.add(0, "");
 		namespaceSet.add(1, "Talk");
 
 		writer = new MockDumpWriter();
-		filter = new NotalkFilter(writer);
+		try {
+			filter = new ListFilter(writer,
+					"src/test/resources/filters/listfilter.txt");
+		} catch (IOException e) {
+			LOG.error("Problem reading exact list filter data", e);
+		}
 	}
 
 	@Test
@@ -49,7 +53,37 @@ public class NotalkFilterTest {
 	}
 
 	@Test
+	public void testTalkPagePermitted() {
+		Page page = new Page();
+		page.Title = new Title(1, "permitted", namespaceSet);
+
+		try {
+			filter.writeStartPage(page);
+			filter.writeEndPage();
+		} catch (IOException e) {
+			LOG.error("Problem writing revision", e);
+		}
+
+		assertEquals(true, writer.isPagePassed());
+	}
+
+	@Test
 	public void testPageFiltered() {
+		Page page = new Page();
+		page.Title = new Title(0, "filtered", namespaceSet);
+
+		try {
+			filter.writeStartPage(page);
+			filter.writeEndPage();
+		} catch (IOException e) {
+			LOG.error("Problem writing revision", e);
+		}
+
+		assertEquals(false, writer.isPagePassed());
+	}
+
+	@Test
+	public void testTalkPageFiltered() {
 		Page page = new Page();
 		page.Title = new Title(1, "filtered", namespaceSet);
 
