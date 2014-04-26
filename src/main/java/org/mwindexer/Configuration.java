f0dfcbb3f56a34437a1696fa0476f38422020855
test 0101 <test72157@gmail.com>
2014-04-25 21:08:55 -0400

    some changes from testing

--- src/main/java/org/mwindexer/Configuration.java
+++ src/main/java/org/mwindexer/Configuration.java
@@ -5,12 +5,11 @@ import java.io.InputStream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-
 @Component
 public class Configuration {
 
 	private InputStream inputStream;
-	
+
 	private String solrURL;
 
 	@Bean
@@ -21,12 +20,12 @@ public class Configuration {
 	public void setInputStream(InputStream inputStream) {
 		this.inputStream = inputStream;
 	}
-	
+
 	@Bean
 	public String getSolrURL() {
 		return solrURL;
 	}
-	
+
 	public void setSolrURL(String url) {
 		this.solrURL = url;
 	}
