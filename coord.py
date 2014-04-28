345f56d38bd09cd7e2b6fdd309036b1e17e448e9
Tim Stoner <timothy.m.stoner@gmail.com>
2014-04-28 15:20:02 -0400

    added python scripts to extract coord templates and category links from wiki dump in order to look at real data

---
+++ coord.py
@@ -0,0 +1,17 @@
+import re
+
+infile = open('enwiki-20140304-pages-articles.xml', 'r')
+outfile = open('coord.txt', 'w')
+
+pattern = re.compile('\{\{coord.*\}\}', re.IGNORECASE)
+i = 0
+for line in infile:
+  i += 1
+  result = re.search(pattern, line)
+  if result is not None:
+    outfile.write(result.group())
+    outfile.write("\n")
+  if i % 10000 == 0:
+    print i
+
+
